0374026796962bdfe52608eca83b3c9f27a5016e
AbrusDev <abrusdev@gmail.com>
2020-07-19 22:57:15 +0500

    current live screen

---
+++ app/src/main/java/com/snatap/myway/ui/screens/main/home/live/CurrentLiveScreen.kt
@@ -0,0 +1,12 @@
+package com.snatap.myway.ui.screens.main.home.live
+
+import com.snatap.myway.R
+import com.snatap.myway.base.BaseFragment
+
+class CurrentLiveScreen : BaseFragment(R.layout.screen_current_live){
+
+    override fun initialize() {
+
+    }
+
+}
