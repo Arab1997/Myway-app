77d27124a64905e1d76a70a4d7510fac74cf0367
AbrusDev <abrusdev@gmail.com>
2020-07-24 20:59:30 +0500

    (fixed UI icons) chat screen

--- app/src/main/java/com/snatap/myway/ui/screens/main/home/chat/ChatWithUserScreen.kt
+++ app/src/main/java/com/snatap/myway/ui/screens/main/home/chat/ChatWithUserScreen.kt
@@ -4,7 +4,7 @@ import com.snatap.myway.R
 import com.snatap.myway.base.BaseFragment
 
 class ChatWithUserScreen : BaseFragment(R.layout.screen_chat_with_user){
-    override fun initialize() {
+     override fun initialize() {
     }
 
 }
