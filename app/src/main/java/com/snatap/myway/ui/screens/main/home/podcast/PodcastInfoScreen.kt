92966e54cecef0e3c77491b711f97d12410922b4
AbrusDev <abrusdev@gmail.com>
2020-08-12 01:56:40 +0500

    music item alignment on MainScreen

--- app/src/main/java/com/snatap/myway/ui/screens/main/home/podcast/PodcastInfoScreen.kt
+++ app/src/main/java/com/snatap/myway/ui/screens/main/home/podcast/PodcastInfoScreen.kt
@@ -1,12 +1,24 @@
 package com.snatap.myway.ui.screens.main.home.podcast
 
+import android.media.MediaPlayer
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.snatap.myway.R
 import com.snatap.myway.base.BaseFragment
 import com.snatap.myway.ui.adapters.PodcastChapterAdapter
 import kotlinx.android.synthetic.main.content_rounded_toolbar_info.*
 import kotlinx.android.synthetic.main.screen_podcast_info.*
+import kotlinx.android.synthetic.main.screen_podcast_play.*
+import java.util.*
 
 class PodcastInfoScreen() : BaseFragment(R.layout.screen_podcast_info) {
+
+    private var mediaPlayer: MediaPlayer? = null
+    private var runnable: Runnable? = null
+    private var handler: Handler? = null
 
     companion object{
         private var txtTitle: String? = null
@@ -14,10 +26,53 @@ class PodcastInfoScreen() : BaseFragment(R.layout.screen_podcast_info) {
             this.txtTitle = txtTitle
             return PodcastInfoScreen()
         }
+
+        public fun stringForTime(timeMs: Int): String? {
+            val mFormatter: Formatter
+            val mFormatBuilder: StringBuilder = StringBuilder()
+            mFormatter = Formatter(mFormatBuilder, Locale.getDefault())
+            val totalSeconds = timeMs / 1000
+            val seconds = totalSeconds % 60
+            val minutes = totalSeconds / 60 % 60
+            val hours = totalSeconds / 3600
+            mFormatBuilder.setLength(0)
+            return if (hours > 0) {
+                mFormatter.format("%d:%02d:%02d", hours, minutes, seconds).toString()
+            } else {
+                mFormatter.format("%02d:%02d", minutes, seconds).toString()
+            }
+        }
     }
 
     override fun initialize() {
         initViews()
+        onReceiveData()
+    }
+
+    private fun onReceiveData(){
+        viewModel.data.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            if (it is Bundle){
+                val bundle: Bundle = it
+
+                val time = bundle.getInt("time")
+                mediaPlayer = MediaPlayer.create(requireContext(), R.raw.musicc)
+                mediaPlayer!!.seekTo(time)
+                mediaPlayer!!.setOnPreparedListener {
+                    mediaPlayer!!.start()
+                    changeSeekBar()
+                }
+                handler = Handler()
+            }
+        })
+    }
+
+
+    private fun changeSeekBar() {
+
+        if (mediaPlayer!!.isPlaying) {
+            runnable = Runnable { changeSeekBar() }
+            handler!!.postDelayed(runnable, 50)
+        }
     }
 
     private fun initViews() {
@@ -29,9 +84,16 @@ class PodcastInfoScreen() : BaseFragment(R.layout.screen_podcast_info) {
         info.setBackgroundResource(R.drawable.ic_mark_white)
 
         recyclerChapters.adapter = PodcastChapterAdapter {
+            if (mediaPlayer != null)
+                mediaPlayer!!.stop()
             addFragment(PodcastPlayScreen())
         }.apply {
             setData(arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
         }
+
+        lastMusicLayer.setOnClickListener {
+            PodcastPlayScreen.newInstance(mediaPlayer!!.currentPosition)
+        }
     }
+
 }
--- app/src/main/java/com/snatap/myway/ui/screens/main/home/podcast/PodcastPlayScreen.kt
+++ app/src/main/java/com/snatap/myway/ui/screens/main/home/podcast/PodcastPlayScreen.kt
@@ -1,29 +1,57 @@
 package com.snatap.myway.ui.screens.main.home.podcast
 
+import android.annotation.SuppressLint
 import android.media.MediaPlayer
+import android.media.PlaybackParams
+import android.os.Build
+import android.os.Bundle
 import android.os.Handler
 import android.widget.SeekBar
 import com.snatap.myway.R
 import com.snatap.myway.base.BaseFragment
+import com.snatap.myway.ui.screens.main.home.podcast.PodcastInfoScreen.Companion.stringForTime
+import com.snatap.myway.utils.extensions.gone
 import kotlinx.android.synthetic.main.screen_podcast_play.*
-import java.util.*
 
 class PodcastPlayScreen : BaseFragment(R.layout.screen_podcast_play) {
 
     private lateinit var mediaPlayer: MediaPlayer
     private var runnable: Runnable? = null
     private var handler: Handler? = null
+    private var playerSpeed:Float = 1f
+
+    companion object {
+        private var currentPos: Int = 0
+        public fun newInstance(currentPos: Int){
+            this.currentPos = currentPos
+        }
+    }
 
     override fun initialize() {
 
         initViews()
         initClicks()
         seekBarListener()
+    }
 
+    override fun onDestroy() {
+        if (handler != null)
+            handler!!.removeCallbacks(runnable)
+        mediaPlayer.stop()
+        val bundle = Bundle()
+        bundle.putInt("time", mediaPlayer.currentPosition)
+        viewModel.data.value = bundle
+        super.onDestroy()
     }
 
     private fun initViews() {
+
+
         mediaPlayer = MediaPlayer.create(requireContext(), R.raw.musicc)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            mediaPlayer.playbackParams = mediaPlayer.playbackParams.setSpeed(playerSpeed)
+        }
+        mediaPlayer.seekTo(currentPos)
         mediaPlayer.setOnPreparedListener {
             seekBar.max = mediaPlayer.duration
             mediaPlayer.start()
@@ -32,12 +60,16 @@ class PodcastPlayScreen : BaseFragment(R.layout.screen_podcast_play) {
         handler = Handler()
 
         txtEndTime.text = stringForTime(mediaPlayer.duration)
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            speedUpLayer.gone()
+        }
     }
 
-    private fun seekBarListener(){
-        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+    private fun seekBarListener() {
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                if (fromUser){
+                if (fromUser) {
                     mediaPlayer.seekTo(progress)
                 }
             }
@@ -51,49 +83,47 @@ class PodcastPlayScreen : BaseFragment(R.layout.screen_podcast_play) {
         })
     }
 
+    @SuppressLint("SetTextI18n")
     private fun initClicks() {
         play.setOnClickListener {
-            if (mediaPlayer.isPlaying){
+            if (mediaPlayer.isPlaying) {
                 mediaPlayer.pause()
                 play.setImageResource(R.drawable.ic_white_play)
             } else {
                 mediaPlayer.start()
+                changeSeekBar()
                 play.setImageResource(R.drawable.ic_white_pause)
             }
         }
 
         forwardBtn.setOnClickListener {
-            mediaPlayer.seekTo(mediaPlayer.currentPosition+30000)
+            mediaPlayer.seekTo(mediaPlayer.currentPosition + 30000)
         }
 
         backBtn.setOnClickListener {
-            mediaPlayer.seekTo(mediaPlayer.currentPosition-30000)
+            mediaPlayer.seekTo(mediaPlayer.currentPosition - 30000)
+        }
+
+        speedUp.setOnClickListener {
+            if (playerSpeed < 2f)
+                playerSpeed += 0.5f
+            else
+                playerSpeed = 1f
+
+            speedUp.text = "${playerSpeed}X"
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mediaPlayer.playbackParams = mediaPlayer.playbackParams.setSpeed(playerSpeed)
+            }
         }
     }
 
-    private fun changeSeekBar(){
+    private fun changeSeekBar() {
         txtCurrentTime.text = stringForTime(mediaPlayer.currentPosition)
         seekBar.progress = mediaPlayer.currentPosition
 
-        if (mediaPlayer.isPlaying){
+        if (mediaPlayer.isPlaying) {
             runnable = Runnable { changeSeekBar() }
             handler!!.postDelayed(runnable, 50)
-        }
-    }
-
-    private fun stringForTime(timeMs: Int): String? {
-        val mFormatter: Formatter
-        val mFormatBuilder: StringBuilder = StringBuilder()
-        mFormatter = Formatter(mFormatBuilder, Locale.getDefault())
-        val totalSeconds = timeMs / 1000
-        val seconds = totalSeconds % 60
-        val minutes = totalSeconds / 60 % 60
-        val hours = totalSeconds / 3600
-        mFormatBuilder.setLength(0)
-        return if (hours > 0) {
-            mFormatter.format("%d:%02d:%02d", hours, minutes, seconds).toString()
-        } else {
-            mFormatter.format("%02d:%02d", minutes, seconds).toString()
         }
     }
 
